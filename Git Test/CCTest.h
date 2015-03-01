955d9c7b8be1f50d8bbb38844466a5aea5640876
masti13harsh <masti13harsh@gmail.com>
2015-03-01 16:45:49 +0530

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Harsh Sapra on 01/03/15.
+//  Copyright (c) 2015 Harsh Sapra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Harsh Sapra on 01/03/15.
+//  Copyright (c) 2015 Harsh Sapra. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
